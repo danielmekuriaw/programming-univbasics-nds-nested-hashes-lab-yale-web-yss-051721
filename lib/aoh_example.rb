93619b8ff12983e2987f7c37a1cd3287d7368639
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-03 01:27:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,8 +12,7 @@ end
 
 def literal_aoh
   [{ name:  "Don Gately", occupation: "Live-in Staff" },
-JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
-PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+{ name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
